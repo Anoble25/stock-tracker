c9b2da8ba87cb66fc11b224b70ce504b9560013b
ANoble25 <AWGR-Alex@comcast.net>
2018-06-30 22:52:50 -0700

    add image loader and make stock graphic show

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import stockGraphic from '../assets/images/stockgraphic.jpg';
 
 function Header(){
+  var stockGraphicStyle = {
+    width: '200px'
+  };
   return (
     <div>
+      <img style={stockGraphicStyle} src={stockGraphic}/>
       <h1>Stock Tracker</h1>
       <Link to="/">Home</Link> | <Link to="/newstock">Create new stock list</Link>
     </div>
